4f2e122848ebdbb8ce8a8ef2d09914c8d38153f6
getssh <gtamrat33@gmail.com>
2023-02-02 00:32:59 +0300

    Update js file for daynamcity and Add js linter

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-const navDeskTop = document.querySelector('.tool-bar')
-const humBurger = document.querySelector('.fa-bars')
+const dropMob = document.querySelector('.tool-bar-mob');
+const navDeskTop = document.querySelector('.tool-bar-desk');
+const humBurger = document.querySelector('.test');
+const closeBtn = document.querySelector('.close');
+const navItems = document.querySelectorAll('.nav-item');
 
 humBurger.addEventListener('click', () => {
-  humBurger.style.display = "none"
-})
+  dropMob.style.display = 'block';
+  navDeskTop.style.display = 'none';
+});
+
+closeBtn.addEventListener('click', () => {
+  dropMob.style.display = 'none';
+  navDeskTop.style.display = 'flex';
+});
+
+for (let i = 0; i < navItems.length; i += 1) {
+  navItems[i].addEventListener('click', () => {
+    dropMob.style.display = 'none';
+    navDeskTop.style.display = 'flex';
+  });
+}
