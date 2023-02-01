358602b99e7463b8120f0fddb428b21f2dcd76bc
Daniel Ochuba <danielochuba78@gmail.com>
2023-02-01 18:56:02 +0100

    Create JS file

---
+++ index.js
@@ -0,0 +1,6 @@
+const navDeskTop = document.querySelector('.tool-bar')
+const humBurger = document.querySelector('.fa-bars')
+
+humBurger.addEventListener('click', () => {
+  humBurger.style.display = "none"
+})
