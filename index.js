340cc290fe2b25d58ffc7a9b3fca4d35b2b3bbd8
getssh <gtamrat33@gmail.com>
2023-02-02 22:53:13 +0300

    Add object for projects in index.js

--- index.js
+++ index.js
@@ -20,3 +20,82 @@ for (let i = 0; i < navItems.length; i += 1) {
     navDeskTop.style.display = 'flex';
   });
 }
+
+const projects = [{
+  featured: true,
+  name: "Multi-Post Stories",
+  description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+  image: "./images/ImgPlaceholder.png",
+  technologies: [
+    "css", "html", "bootstrap", "Ruby"
+  ],
+  liveLink: "",
+  sourceLink: ""
+},
+{
+  featured: false,
+  name: "Profesional Art Printing Data",
+  description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+  image: "./images/ImgPlaceholder.png",
+  technologies: [
+    "html", "bootstrap", "Ruby"
+  ],
+  liveLink: "",
+  sourceLink: ""
+},
+{
+  featured: false,
+  name: "Data Dashboard Healthcare",
+  description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+  image: "./images/ImgPlaceholder.png",
+  technologies: [
+    "html", "bootstrap", "Ruby"
+  ],
+  liveLink: "",
+  sourceLink: ""
+},
+{
+  featured: false,
+  name: "Data Dashboard Healthcare",
+  description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+  image: "./images/ImgPlaceholder.png",
+  technologies: [
+    "html", "bootstrap", "Ruby"
+  ],
+  liveLink: "",
+  sourceLink: ""
+},
+{
+  featured: false,
+  name: "Data Dashboard Healthcare",
+  description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+  image: "./images/ImgPlaceholder.png",
+  technologies: [
+    "html", "bootstrap", "Ruby"
+  ],
+  liveLink: "",
+  sourceLink: ""
+},
+{
+  featured: false,
+  name: "Data Dashboard Healthcare",
+  description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+  image: "./images/ImgPlaceholder.png",
+  technologies: [
+    "html", "bootstrap", "Ruby"
+  ],
+  liveLink: "",
+  sourceLink: ""
+},
+{
+  featured: false,
+  name: "Data Dashboard Healthcare",
+  description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+  image: "./images/ImgPlaceholder.png",
+  technologies: [
+    "html", "bootstrap", "Rubyy"
+  ],
+  liveLink: "",
+  sourceLink: ""
+}
+]
