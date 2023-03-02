3e6faea3ef60cd4fa4794420ae1b949b349221a5
getssh <gtamrat33@gmail.com>
2023-03-02 21:12:06 +0300

    Update live and github sourcse linkes for project

--- index.js
+++ index.js
@@ -47,8 +47,8 @@ const projects = [{
   technologies: [
     'html', 'bootstrap', 'Ruby',
   ],
-  liveLink: '#',
-  sourceLink: '#',
+  liveLink: 'https://getssh.github.io/web-course-capstone/',
+  sourceLink: 'https://github.com/getssh/web-course-capstone',
   popUpDetail: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
   popUpImage: './images/pop-up.png',
 },
